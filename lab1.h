5935fd4c42852b0da2ad5ac8714fb79f86a21f43
Rboan08 <152340336+Rboan08@users.noreply.github.com>
2025-04-09 18:45:24 -0700

    Update lab1.h

--- lab1.h
+++ lab1.h
@@ -7,4 +7,8 @@
 #define GPIODIR_F (*((volatile uint32_t *)0x4005D400))
 #define GPIODATA_F (*((volatile uint32_t *)0x4005D3FC))
 
+#define GPIODEN_J (*((volatile uint32_t *)0x4006051C))
+#define GPIODIR_J (*((volatile uint32_t *)0x40060400))
+#define GPIOPUR_J (*((volatile uint32_t *)0x40060510))
+
 #endif // __HEADER1_H__
