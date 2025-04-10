e43691609c3afa18be7937d972fe48057d21ae93
Rboan08 <152340336+Rboan08@users.noreply.github.com>
2025-04-09 18:45:02 -0700

    Update main.c

--- main.c
+++ main.c
@@ -9,6 +9,13 @@ int main(void) {
   GPIODEN_F = 0x1; // Set PF0 to enable digital function
   GPIODIR_F = 0x1; // Set PF0 to an output
   GPIODATA_F = 0x1; // Set PF0 to 1
+  
+  RCGCGPIO |= 0x100; // Enable PortJ GPIO by setting bit 8 to 1
+  delay++;
+  delay++;
+  GPIODEN_J = 0x3; // Set PJ0 and PJ1 to enable digital function
+  GPIODIR_J = 0x0; // Set PJ0 and PJ1 explicitly to be input
+  GPIOPUR_J = 0x3; // Set PJ0 and PJ1 to enable pull-up resistors
   while (1) {}
   return 0;
 }
