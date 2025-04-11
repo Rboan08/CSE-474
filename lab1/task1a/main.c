2b3d8d1c9ea41e00c822389072ec6f6e8c65e1d7
Rboan08 <152340336+Rboan08@users.noreply.github.com>
2025-04-11 10:29:24 -0700

    Cleanup lab1a main.c

--- lab1/task1a/main.c
+++ lab1/task1a/main.c
@@ -14,13 +14,6 @@ int main(void) {
   delay++;
   GPIODEN_N = 0x3; // PN0/PN1 to digital function
   GPIODIR_N = 0x3; // PN0/PN1 to output
-  
-  RCGCGPIO |= 0x100; // Enable PortJ GPIO by setting bit 8 to 1
-  delay++;
-  delay++;
-  GPIODEN_J = 0x3; // Set PJ0 and PJ1 to enable digital function
-  GPIODIR_J = 0x0; // Set PJ0 and PJ1 explicitly to be input
-  GPIOPUR_J = 0x3; // Set PJ0 and PJ1 to enable pull-up resistors
   while (1) {
     GPIODATA_N |= 0x2; // Turn on PN1 (D1)
     for (int j=0;j<1000000;j++) {}
